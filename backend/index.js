971e473e2431caea03f2a8041a62ee62ea3efebf
root <root@LAPTOP-5IHK81KM.localdomain>
2025-09-20 12:13:27 +0300

    added static frontend to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 
-const cors = require('cors')
 const express = require("express");
 const morgan = require('morgan')
 const app = express();
 
 app.use(express.json())
-app.use(cors())
+app.use(express.static('dist'))
 
 
 morgan.token('bodyData', function (req, res) { 
